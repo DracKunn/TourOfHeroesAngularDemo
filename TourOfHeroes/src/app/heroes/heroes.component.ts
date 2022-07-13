269a5dd61e6992aa7c0108059b0c21711500a91e
DracKunn <brecht.vanrie@gmail.com>
2022-07-13 14:53:58 +0200

    tour of heroes pt3

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,8 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  heroes = HEROES;
 
 
   constructor() {
@@ -19,6 +18,10 @@ export class HeroesComponent implements OnInit {
   ngOnInit()
     :
     void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
---
+++ TourOfHeroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Craig Christ'},
+  {id: 13, name: 'Rubber Person'},
+  {id: 14, name: 'Freedom Guy'},
+  {id: 15, name: 'Drug-free Boy'},
+  {id: 16, name: 'Ol\' Dood'},
+  {id: 17, name: 'Single Mom'},
+  {id: 18, name: 'Florida Man'},
+  {id: 19, name: 'Pablo'},
+  {id: 20, name: 'Immigration Dude'},
+  {id: 21, name: 'Absentee Dad'},
+  {id: 22, name: 'Homeless Woman'}
+]
